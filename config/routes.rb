a20ca84f60751e11be0a09a262519a278dc72f30
t-ueta <ueta7651@gmail.com>
2019-10-02 15:54:47 +0900

    create_user_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tests#index'
+  root to: 'users#new'
   resources :tests
+  resources :users, only: [:new] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
