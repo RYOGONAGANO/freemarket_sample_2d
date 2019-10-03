d8ef5a9bfe6c4774396c4ad9798976978570dac5
kaitokwt <vitesthedust@gmail.com>
2019-10-03 15:43:35 +0900

    create/products/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#index'
+  root to: 'products#show'
   resources :tests
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
