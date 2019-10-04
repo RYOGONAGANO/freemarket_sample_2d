9b5d0c527a2553847068e12bfe8337435b6a3ca5
tsuzumi07 <tuzumi.0107@gmail.com>
2019-10-04 17:44:51 +0900

    first_setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
+  def index
+    render "users/member-information_input"
+  end
+
   def new
     render partial: "users/login-page"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#new'
+  root to: 'users#index'
   resources :tests
   resources :users
   
