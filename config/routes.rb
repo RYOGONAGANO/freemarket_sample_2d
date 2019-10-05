d2b40059ac05cf73f96536fbed1aa0de3f1fb44f
RYOGONAGANO <rockryobadas@icloud.com>
2019-10-05 18:17:18 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#show'
   resources :tests
-  resources :users
+  resources :users do
+    collection do
+      get :profile
+      get :authentication
+    end
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
