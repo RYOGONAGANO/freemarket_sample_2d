79111b154579d453b675d029d2be9a56b080f0ed
kaitokwt <vitesthedust@gmail.com>
2019-10-08 16:31:43 +0900

    edit/validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+  belongs_to :user, dependent: destroy
+
+  validates :prefectures, presence: true
+  validates :city, presence: true
+  validates :address, precence: true
 end
