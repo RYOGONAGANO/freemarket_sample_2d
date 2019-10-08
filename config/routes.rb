1e1aaba916ad086fe97b528e71c2f23d59778671
kaitokwt <vitesthedust@gmail.com>
2019-10-08 16:40:42 +0900

    chage/name

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get :authentication
       get :login
       get :logout
+      get :member_information_input
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
