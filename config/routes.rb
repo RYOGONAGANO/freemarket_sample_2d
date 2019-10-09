276778a24f4c825d5ad2c2d0f2587e62278f03d4
komatsu-naoki <base.ball44.kn.2018@icloud.com>
2019-10-09 22:59:35 +0900

    link_retouch

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
       get :logout
       get :registrationNewmenbar
       get :address
+      get :mypage
     end
   end
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
