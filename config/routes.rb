ae1927eb03193f4fa0d560d77ad69c1b7a8d5765
t-ueta <ueta7651@gmail.com>
2019-10-09 17:57:46 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       get :logout
       get :registrationNewmenbar
       get :address
+      get :buyComplete
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
