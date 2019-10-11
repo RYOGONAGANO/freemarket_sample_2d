0ccddc8a08dbfb1b5995b3ff19021b489020b137
kaitokwt <vitesthedust@gmail.com>
2019-10-11 12:25:18 +0900

    fix/user/controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,25 +14,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    params[:user][:birthday] = birthday_join
-    binding.pry
     @user = User.create(user_params)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email, user_detail_attributes: [:first_name, :last_name, :first_name_kata, :last_name_kata, :birthday ])
   end
 
-  def birthday_join
-    date = params[:user][:birthday]
 
-    if date["birthday(1i)"].empty? && date["birthday(2i)"].empty? && date["birthday(3i)"].empty?
-      return
-    end
-
-    Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
-
-  end
 end
