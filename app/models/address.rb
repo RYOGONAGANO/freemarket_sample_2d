15c0b38e7516840697eb935b9241700df9942238
kaitokwt <vitesthedust@gmail.com>
2019-10-12 19:10:02 +0900

    create/activehash

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,10 @@
 class Address < ApplicationRecord
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
-  validates :postal_code, presence: true
-  validates :prefectures, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
+  belongs_to :user
+  validates  :postal_code, presence: true
+  validates  :prefecture_id, presence: true
+  validates  :city, presence: true
+  validates  :address, presence: true
 end
