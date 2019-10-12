be429ab23747dd411624f8135e2aa777174168a6
kaitokwt <vitesthedust@gmail.com>
2019-10-12 18:59:02 +0900

    update/column

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_020013) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefectures", default: 0, null: false
+    t.integer "prefecture_id", default: 0, null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building"
