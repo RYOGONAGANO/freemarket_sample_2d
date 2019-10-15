3cf61f89c57958997fe574c3500208789fe33a91
kaitokwt <vitesthedust@gmail.com>
2019-10-15 19:42:36 +0900

    test/model/userDetail

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -3,8 +3,8 @@ class UserDetail < ApplicationRecord
 
   validates :first_name, presence: true, length: { maximum: 35 }
   validates :last_name,  presence: true, length: { maximum: 35 }
-  validates :first_name_kata, presence: true
-  validates :last_name_kata, presence: true
+  validates :first_name_kata, presence: true, length: { maximum: 35 }, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kata, presence: true, length: { maximum: 35 }, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
   has_one_attached :avatar
