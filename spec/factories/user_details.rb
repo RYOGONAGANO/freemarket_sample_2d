a63cf57ea24c3822757ee04937c63d1b6c916f25
kaitokwt <vitesthedust@gmail.com>
2019-10-15 20:19:28 +0900

    fix/indent

--- spec/factories/user_details.rb
+++ spec/factories/user_details.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user_detail do
     gimei = Gimei.name
-    first_name { gimei.last.kanji }
-    last_name { gimei.first.kanji }
+    first_name      { gimei.last.kanji }
+    last_name       { gimei.first.kanji }
     first_name_kata { gimei.last.katakana }
-    last_name_kata { gimei.first.katakana }
-    birthday { Faker::Date.birthday(min_age: 18, max_age: 65).to_s(:ja) }
+    last_name_kata  { gimei.first.katakana }
+    birthday        { Faker::Date.birthday(min_age: 18, max_age: 65).to_s(:ja) }
     user
   end
 end
