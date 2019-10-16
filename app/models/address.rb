aa7cfccb0f2fb4e0c89a2841c2e0423400098fc7
kaitokwt <vitesthedust@gmail.com>
2019-10-16 10:23:38 +0900

    test/model/address

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   belongs_to :user
-  validates  :postal_code, presence: true
+  validates  :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates  :prefecture_id, presence: true
   validates  :city, presence: true
   validates  :address, presence: true
