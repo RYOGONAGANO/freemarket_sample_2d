a5febad6ec55a60e2f0832e8d90aa570036fcfed
komatsu-naoki <base.ball44.kn.2018@icloud.com>
2019-10-18 19:02:45 +0900

    active_hash(exhibit)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :buyer, class_name: "User"
   belongs_to :exhibitor, class_name: "User"
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :product_category
+  belongs_to_active_hash :product_status
+  belongs_to_active_hash :delivery_fee
+
 end
