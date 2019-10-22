4d1a2ca966ea8eae64f11c31b01bef5cb5d3b5f8
RYOGONAGANO <rockryobadas@icloud.com>
2019-10-22 13:18:58 +0900

    view

--- config/initializers/payjp.rb
+++
@@ -1,6 +0,0 @@
-# Payjp.configure do |config|
-#   config.site_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
-#   config.secret_key = Rails.application.credentials.payjp[:PAYJP_KEY]
-#   # Uncomment the following line if you are using a proxy server:
-#   # config.proxy = 'http://myproxy.com.au:8080'
-# end
