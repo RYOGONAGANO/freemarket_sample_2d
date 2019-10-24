f9b961655c14cf4ed0ca5fdf3a07691222f177ad
kaitokwt <vitesthedust@gmail.com>
2019-10-24 20:48:02 +0900

    update/sidemenu/hover/color

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,4 @@
 $(function(){
-  $(".mypage-menu-list__line").hover(function() {
-    $(this).css('background', '#eee');
-  }, function() {
-    $(this).css('background', '');
-  });
-
   var link = $(".mypage-menu-list__line");
   var currentUrl = location.pathname;
   link.each(function() {
