705f35141166a9b1fa74b02023372e62923db85a
kaitokwt <vitesthedust@gmail.com>
2019-10-30 17:59:21 +0900

    attach/images

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   belongs_to :exhibitor, class_name: "User"
 
+  has_many_attached :images
+
+
   def previous
     Product.order(id: "DESC").find_by("id < ?", self.id)
   end
