279e087622f47efab75226460e56e1b0e3ec17fc
kaitokwt <vitesthedust@gmail.com>
2019-11-01 19:58:15 +0900

    function/dropzone

--- app/assets/javascripts/channels/dropzone-product.js
+++ app/assets/javascripts/channels/dropzone-product.js
@@ -1,25 +1,6 @@
 jQuery(document).ready(function() {
-  //   $("#drop").dropzone({
-  //   url: "/products",
-  //   paramName: "product[images][]",
-  //   uploadMultiple: true,
-  //   addRemoveLinks: true,
-  //   dictRemoveFile: "削除",
-  //   previewTemplate: `
-  //       <div class=\"dz-preview dz-file-preview\">
-  //         <div class=\"dz-image\">
-  //           <img data-dz-thumbnail />
-  //         </div>
-  //         <div class="upload-btn">
-  //           <a href="" class="upload-edit">編集</a>
-  //       </div>`,
-  //   maxFiles: 10,
-  //   maxFilesize: 10,
-  //   autoProcessQueue: false,
-  //   dictDefaultMessage: "ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード"
-  // });
   var myDropzone = new Dropzone("#dropzone", {
-    url: "/products/image",
+    url: "/products",
     paramName: "product[images]",
     uploadMultiple: true,
     addRemoveLinks: true,
@@ -40,11 +21,20 @@ jQuery(document).ready(function() {
     parallelUploads: 10,
     thumbnailHeight: null,
     thumbnailWidth: null,
-    previewsContainer: ".previews",
+    previewsContainer: '.previews',
     init: function() {
-      if ( !this.element.querySelector(".dz-message") ) {
-        this.element.appendChild(Dropzone.createElement("<div class=\"dz-default dz-message\"><span>" + this.options.dictDefaultMessage + "</span></div>"));
+      if (!this.element.querySelector(".dz-message")) {
+        this.element.appendChild(
+          Dropzone.createElement(
+            '<div class="dz-default dz-message"><span>' +
+              this.options.dictDefaultMessage +
+              "</span></div>"
+          )
+        );
       }
+    },
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
     }
   });
 
@@ -52,29 +42,39 @@ jQuery(document).ready(function() {
     myDropzone.processQueue();
   });
 
-  const target = document.getElementById('previews');
-  const add_target = document.getElementById('dropzone');
+  const target = document.getElementById("previews");
+  const add_target = document.getElementById("dropzone");
   var count = 1;
   var befor_size = -1;
-  var observer = new MutationObserver(function(){
+  var observer = new MutationObserver(function() {
     var size = $(".dz-preview").length;
-    console.log(size);
-    console.log(befor_size);
-    if( size < befor_size ) {
+
+    if (size > 9) {
+      $("#dropzone").css('display', 'none');
+    } else if (size > 5) {
+      $("#dropzone").css('top', '174px');
+      $("#dropzone").css('display', '');
+    } else {
+      $("#dropzone").css('top', '');
+    };
+
+
+    if (size < befor_size ) {
       count--;
       add_target.classList.remove(`item${count}`);
+      if (count < 1 ) count = 5;
+      add_target.classList.add(`item${count - 1}`);
       befor_size = size;
-    } else if( size > befor_size){
-      add_target.classList.remove(`item${count}`);
+    } else if (size > befor_size ) {
+      add_target.classList.remove(`item${count - 1}`);
+      if (count > 4) count = 0;
       add_target.classList.add(`item${count}`);
       count++;
       befor_size = size;
     } else {
-      alert('エラーです。');
-    };
-    console.log(count);
+      alert("エラーです。");
+    }
   });
-
 
   const config = {
     attributes: true,
@@ -82,15 +82,8 @@ jQuery(document).ready(function() {
     characterData: true
   };
 
-  console.log(add_target);
   observer.observe(target, config);
 
   // observer.disconnect();
 
-
-
-  // $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-  //   var token = $('meta[name="csrf-token"]').attr('content');
-  //   return jqXHR.setRequestHeader('X-CSRF-Token', token);
-  // });
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,4 @@
 class ProductsController < ApplicationController
-  # protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token, only: [:image]
 
   def index
     @products = Product.where("id <= ?", 10)
@@ -9,13 +7,9 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
-  #  FIXME:ランダムで起動してる。
+
   def create
-    binding.pry
-  end
-  #  FIXME: befor_actionで呼び出してifでやるかやらないかを決める
-  def image
-    session[:images] = params[:product][:images]
+    session[:images] = params[:product][:images] unless session[:images]
   end
 
   def show
