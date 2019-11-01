978572db136413d2751e5b9eb3ad224f50df3774
kaitokwt <vitesthedust@gmail.com>
2019-11-01 20:08:38 +0900

    fix/routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,6 @@ Rails.application.routes.draw do
   end
   resources :products do
     resources :buys, only: [:new, :create]
-    collection do
-      post :image
-    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
