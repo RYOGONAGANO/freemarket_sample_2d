5db84075943d52d20a1adf67d0764ae97a9ea0a9
t-ueta <ueta7651@gmail.com>
2019-11-04 19:01:35 +0900

    fix_view_sallingProductsList

--- app/controllers/saling_products_controller.rb
+++ app/controllers/saling_products_controller.rb
@@ -3,9 +3,3 @@ class SalingProductsController < ApplicationController
     @products = Product.where(exhibitor_id: current_user.id)
   end
 end
-
-# @user = current_user
-# @products = @user.products
-
-# @products = Product.where("id <= ?", 10)
-# exhibitor_id
