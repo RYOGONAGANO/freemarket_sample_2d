dde3fbb525953ca29ac8d45b9ecce53906e45f24
komatsu-naoki <base.ball44.kn.2018@icloud.com>
2019-11-05 22:18:49 +0900

    pankuzu

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,6 @@ class Product < ApplicationRecord
   belongs_to :exhibitor, class_name: "User"
   belongs_to :category
   belongs_to_active_hash :prefecture
-  has_one_attached :image
 
   enum status: {新品、未使用: 1, 未使用に近い: 2, 目立った傷や汚れなし: 3,
                 やや傷や汚れあり: 4, 傷や汚れあり: 5, 全体的に状態が悪い: 6}
---
+++ config/breadcrumbs.rb
@@ -0,0 +1,36 @@
+crumb :root do
+  link 'メルカリ', root_path
+end
+
+crumb :user_show do
+  link "マイページ", users_path
+  parent :root
+end
+
+crumb :users_logout do
+  link "ログアウト", logout_users_path
+  parent :user_show
+end
+
+crumb :users_profilechange do
+  link "本人情報の登録", profilechange_users_path
+  parent :user_show
+end
+
+crumb :cards_index do
+  link "支払い方法", cards_path
+  parent :user_show
+end
+
+crumb :users_profile do
+  link "プロフィール", profile_users_path
+  parent :user_show
+end
+
+crumb :products_name do |product|
+  link product.name
+  parent :root
+end
+
+
+
