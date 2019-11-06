551ee6c5176135bb4fe9ddd88f8e58f098e35899
kaitokwt <vitesthedust@gmail.com>
2019-11-06 21:10:27 +0900

    update/model/product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,26 +1,26 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :buyer, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: true
   belongs_to :exhibitor, class_name: "User"
   belongs_to :category
   belongs_to_active_hash :prefecture
-  has_one_attached :image
+  has_many_attached :images
 
-  enum status: {新品、未使用: 1, 未使用に近い: 2, 目立った傷や汚れなし: 3,
-                やや傷や汚れあり: 4, 傷や汚れあり: 5, 全体的に状態が悪い: 6}
+  enum status: {"新品、未使用": 1, "未使用に近い": 2, "目立った傷や汚れなし": 3,
+                "やや傷や汚れあり": 4, "傷や汚れあり": 5, "全体的に状態が悪い": 6}, _suffix: true
 
   enum fee: {"送料込み(出品者負担)": 1, "着払い(購入者負担)": 2}
 
-  enum shippingday: {"1~2日で発送": 1, "2~3日で発送": 2, "4~7日で発送": 3}
+  enum shipping_date: {"1~2日で発送": 1, "2~3日で発送": 2, "4~7日で発送": 3}
 
-  enum shippingmethod1: {未定: 1, らくらくメルカリ便: 2, ゆうメール: 3,
+  enum shipping_method1: {未定: 1, らくらくメルカリ便: 2, ゆうメール: 3,
                         レターパック: 4, "普通郵便(定形、定形外)": 5, クロネコヤマト: 6,
                         ゆうパック: 7, クリックポスト: 8, ゆうパケット: 9},_prefix: true
 
-  enum shippingmethod2: {未定: 1,  クロネコヤマト: 6 ,ゆうパック: 7, ゆうメール: 3,},_prefix: true
+  enum shipping_method2: {未定: 1,  クロネコヤマト: 6 ,ゆうパック: 7, ゆうメール: 3,},_prefix: true
 
   enum size: {"XXS以下": 1, "XS(SS)": 2, "M": 3, "L": 4,
-              "XL(LL)": 5, "2XL(3L)": 6, "3XL(4L)": 7, 
+              "XL(LL)": 5, "2XL(3L)": 6, "3XL(4L)": 7,
               "4XL(5L)以上": 8, "FREE SIZE": 9}
 
 
@@ -29,9 +29,8 @@ class Product < ApplicationRecord
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, length: { in: 0..40 }
   validates :description, length: { in: 0..999 }
-  validates :image, :name, :description, :category, :status, :charge, :shipping_area, :shipping_date, :price, presence: true
+  validates :name, :description, :category, :status, :fee, :shipping_area, :shipping_date, :price, presence: true
 
-  has_many_attached :images
 
 
   def previous
