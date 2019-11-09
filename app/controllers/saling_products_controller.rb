90bd0c32e91f1864af003572603108d74f5697bb
kaitokwt <vitesthedust@gmail.com>
2019-11-09 18:52:54 +0900

    update/saling_product

--- app/controllers/saling_products_controller.rb
+++ app/controllers/saling_products_controller.rb
@@ -1,5 +1,5 @@
 class SalingProductsController < ApplicationController
   def index
-    @products = Product.where(exhibitor_id: current_user.id)
+    @products = Product.with_attached_images.where(exhibitor_id: current_user.id)
   end
 end
