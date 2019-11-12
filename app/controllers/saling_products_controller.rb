5162fb31765bd67883a12177c6caa006990d82e9
t-ueta <ueta7651@gmail.com>
2019-11-12 17:12:46 +0900

    Update saling_products_controller.rb

--- app/controllers/saling_products_controller.rb
+++ app/controllers/saling_products_controller.rb
@@ -1,17 +1,19 @@
 class SalingProductsController < ApplicationController
+  before_action :set_product, only: [:show, :destroy]
   def index
     @products = Product.with_attached_images.where(exhibitor_id: current_user.id)
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    if @product.exhibitor_id == current_user.id
-      @product.destroy
-    end
+    @product.exhibitor_id == current_user.id
+    render user_saling_product_path(current_user,params[:id]) unless @product.destroy
   end
-  
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
